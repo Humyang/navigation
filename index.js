8405fdc084c64ca9a6bd48f9157a9e1f1c175195
Yangxg <jessebutchman@gmail.com>
2018-04-19 21:12:28 +0800

    更新

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var koa = require('koa')
 var serve = require('koa-static');
 const app = new koa();
-const PORT = 8000;
+const PORT = 8600;
 app.use(serve("./static",{maxage:3153600000}))
 app.listen(PORT);
 console.log('listener on ',PORT)
